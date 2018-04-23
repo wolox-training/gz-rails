899ac91b448884b2787bf33898a142e7dc8a1a29
Gonzalo Zamudio <gonzalo.zamudio@wolox.com.ar>
2018-04-23 15:56:35 -0300

    Added auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :validatable
   validates :first_name, :last_name, presence: true
+
+  include DeviseTokenAuth::Concerns::User
 end
